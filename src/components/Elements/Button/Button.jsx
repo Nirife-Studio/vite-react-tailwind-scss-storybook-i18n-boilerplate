fd0debb56ff5aaa6c873a46a559cf31bf7001080
baovit72 <17520267@gm.uit.edu.vn>
2023-07-27 00:09:17 +0700

    WIP: add ignore files

--- src/components/Elements/Button/Button.jsx
+++ src/components/Elements/Button/Button.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import PropTypes from 'prop-types';
+
 function Button({ content }) {
   return <div>{content}</div>;
 }
+
+Button.propTypes = {
+  content: PropTypes.string.isRequired,
+};
 
 export default Button;
